cb4d1f0bded2d42e487c0afd8e4719250a312af0
Ray WANG <admin@shunix.com>
2014-02-11 01:10:15 +0800

    Add a library to show the charts

---
+++ pushups/src/main/java/com/shunix/dailypushups/fragments/LineGraphFragment.java
@@ -0,0 +1,19 @@
+package com.shunix.dailypushups.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author Ray WANG <admin@shunix.com>
+ * @version 1.0.0
+ * @since Feb 10th, 2014
+ */
+public class LineGraphFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
--- pushups/src/main/java/com/shunix/dailypushups/fragments/PanelFragment.java
+++ pushups/src/main/java/com/shunix/dailypushups/fragments/PanelFragment.java
@@ -1,6 +1,5 @@
 package com.shunix.dailypushups.fragments;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
