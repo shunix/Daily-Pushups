f39c384ec0e07f3c531fa7591a34723c0b775d03
Ray WANG <admin@shunix.com>
2014-02-17 20:56:19 +0800

    Bug fixed

--- pushups/src/main/java/com/shunix/dailypushups/ui/SeekBarPreference.java
+++ pushups/src/main/java/com/shunix/dailypushups/ui/SeekBarPreference.java
@@ -3,9 +3,12 @@ package com.shunix.dailypushups.ui;
 import android.content.Context;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
+import com.shunix.dailypushups.BuildConfig;
 import com.shunix.dailypushups.R;
 
 /**
@@ -24,6 +27,10 @@ public class SeekBarPreference extends DialogPreference implements SeekBar.OnSee
      * Indicates the current seek bar value.
      */
     private int barValue = 0;
+    /**
+     * Show the current seconds.
+     */
+    private TextView textView;
 
     public SeekBarPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -33,15 +40,25 @@ public class SeekBarPreference extends DialogPreference implements SeekBar.OnSee
     protected void onBindDialogView(View view) {
         super.onBindDialogView(view);
         seekBar = (SeekBar) view.findViewById(R.id.seekBar);
+        textView = (TextView) view.findViewById(R.id.textView);
         seekBar.setOnSeekBarChangeListener(this);
-        seekBar.setProgress(barValue);
+        seekBar.setProgress(getPersistedInt(5));
+        textView.setText((getPersistedInt(5) / 10) + "s");
     }
 
     @Override
     protected void onDialogClosed(boolean positiveResult) {
         super.onDialogClosed(positiveResult);
         if (positiveResult) {
-            persistInt(barValue);
+            if ((barValue / 10) < 1) {
+                // If set to zero, set to default.
+                if (BuildConfig.DEBUG) {
+                    Log.d("Bar Value", String.valueOf(barValue));
+                }
+                persistInt(50);
+            } else {
+                persistInt(barValue);
+            }
         }
     }
 
@@ -57,5 +74,6 @@ public class SeekBarPreference extends DialogPreference implements SeekBar.OnSee
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        textView.setText((seekBar.getProgress() / 10) + "s");
     }
 }
